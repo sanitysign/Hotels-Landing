522ed6460af4473b0b680bc9b8a6a4694079aeb9
sanitysign <sanitysign@gmail.com>
2020-12-27 15:51:29 +0300

    map dynamically loaded

--- src/scripts/addYandexMaps.js
+++ src/scripts/addYandexMaps.js
@@ -1,4 +1,27 @@
-ymaps.ready(init)
+const map = document.querySelector('.map')
+const clientHieght = document.documentElement.clientHeight
+let mapLoaded = false
+
+loadMap()
+
+window.onscroll = () => {
+  if (mapLoaded) return
+  loadMap()
+}
+
+function loadMap() {
+
+  if (map.getBoundingClientRect().top - clientHieght < 1000) {
+
+    const script = document.createElement('script')
+    script.defer = true
+    script.src = "https://api-maps.yandex.ru/2.1/?&lang=ru_RU"
+    document.head.append(script)
+
+    script.onload = () => ymaps.ready(init)
+    mapLoaded = true
+  }
+}
 
 function init(){
 
